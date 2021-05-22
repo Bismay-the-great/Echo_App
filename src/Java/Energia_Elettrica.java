774549304c0c98672e2470ea40a039fbebd56f4f
bismaysarao <bismaysinghsarao@gmail.com>
2021-05-22 10:31:35 +0200

    Update Bismay

--- src/Java/Energia_Elettrica.java
+++ src/Java/Energia_Elettrica.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.sql.PseudoColumnUsage;
 
 public class Energia_Elettrica extends JFrame{
 
@@ -26,6 +25,35 @@ public class Energia_Elettrica extends JFrame{
         tableModel.addRow(new Object[]{" Ottobre "});
         tableModel.addRow(new Object[]{" Novembre "});
         tableModel.addRow(new Object[]{" Dicembre "});
+
+        Object e = table.getValueAt(1,1);
+        table.setValueAt(e,1,3);
+        Object f = table.getValueAt(2,1);
+        table.setValueAt(f,2,3);
+        Object g = table.getValueAt(3,1);
+        table.setValueAt(g,3,3);
+        Object h = table.getValueAt(4,1);
+        table.setValueAt(h,4,3);
+        Object i = table.getValueAt(5,1);
+        table.setValueAt(i,5,3);
+        Object l = table.getValueAt(6,1);
+        table.setValueAt(l,6,3);
+        Object m = table.getValueAt(7,1);
+        table.setValueAt(m,7,3);
+        Object n = table.getValueAt(8,1);
+        table.setValueAt(n,8,3);
+        Object o = table.getValueAt(9,1);
+        table.setValueAt(o,9,3);
+        Object p = table.getValueAt(10,1);
+        table.setValueAt(p,10,3);
+        Object q = table.getValueAt(11,1);
+        table.setValueAt(q,11,3);
+        Object r = table.getValueAt(12,1);
+        table.setValueAt(r,12,3);
+
+
+
+
         System.out.println("Table rows = " + table.getRowCount());
         System.out.println("Table Column = " + table.getColumnCount());
         Font font = new Font("New Tegomin", Font.PLAIN ,15);
@@ -38,44 +66,4 @@ public class Energia_Elettrica extends JFrame{
     }
 
 }
-
-
-class Tabella_Energia extends JFrame
-{
-
-}
-
-// CLASS RENDERER
-
-class RowRenderer extends JLabel implements ListCellRenderer{
-
-    // CONSTRUCTOR
-    public RowRenderer(JTable table) {
-
-    }
-
-
-
-
-
-    @Override
-    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-        //create auto generated method
-
-        return null;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
